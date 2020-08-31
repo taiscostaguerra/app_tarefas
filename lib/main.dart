52fb0fffa70a78bcc18ffc787d4f54d3a2f73898
Tais Costa <taiscguerra@gmail.com>
2020-08-31 17:03:40 -0300

    lista e salvamento de dados finalizados

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,31 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final _toDoController = TextEditingController();
+
   List _toDoList = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    _readData().then((data) {
+      setState(() {
+        _toDoList = json.decode(data);
+      });
+    });
+  }
+
+  void _addToDo() {
+    setState(() {
+      Map<String, dynamic> newToDo = Map();
+      newToDo["title"] = _toDoController.text;
+      _toDoController.text = "";
+      newToDo["ok"] = false;
+      _toDoList.add(newToDo);
+      _saveData();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,6 +58,7 @@ class _HomeState extends State<Home> {
             children: <Widget>[
               Expanded(
                 child: TextField(
+                  controller: _toDoController,
                   decoration: InputDecoration(
                     labelText: "Nova Tarefa",
                     labelStyle: TextStyle(color: Colors.blueAccent),
@@ -44,11 +69,32 @@ class _HomeState extends State<Home> {
                 color: Colors.blueAccent,
                 child: Text("ADD"),
                 textColor: Colors.white,
-                onPressed: () {},
+                onPressed: _addToDo,
               ),
             ],
           ),
-        )
+        ),
+        Expanded(
+          child: ListView.builder(
+            padding: EdgeInsets.only(top: 10.0),
+            itemCount: _toDoList.length,
+            itemBuilder: (context, index) {
+              return CheckboxListTile(
+                title: Text(_toDoList[index]["title"]),
+                value: _toDoList[index]["ok"],
+                secondary: CircleAvatar(
+                    child: Icon(
+                        _toDoList[index]["ok"] ? Icons.check : Icons.error)),
+                onChanged: (c) {
+                  setState(() {
+                    _toDoList[index]["ok"] = c;
+                    _saveData();
+                  });
+                },
+              );
+            },
+          ),
+        ),
       ]),
     );
   }
